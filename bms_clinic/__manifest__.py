eebd1bf6a117da6f9ad0f3ce9b759cfb3ffdb9e8
NgocThach Pham Getweb <ngocthach.pham@gmail.com>
2018-07-19 15:19:50 +0700

    Initial commit

---
+++ bms_clinic/__manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Quản lý phòng khám",
+
+    'summary': """
+        Module quản lý phòng khám của BMS TECHNOLOGY""",
+
+    'description': """
+        Module quản lý phòng khám của BMS TECHNOLOGY
+    """,
+
+    'author': "BMS TECHNOLOGY",
+    'website': "http://www.bmstech.io",
+    #support for show when filter App
+    'installable': True,
+    'application': True,
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'BMS Clinic',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'hr', 'crm'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        #'demo/demo.xml',
+    ],
+}
